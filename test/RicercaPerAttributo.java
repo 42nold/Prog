8dde9c164c09a9ff73237dec93d70a139798d3e4
Arnold <Arnold@10.0.91.65>
2018-08-29 11:44:52 +0200

    qfdv

---
+++ test/RicercaPerAttributo.java
@@ -0,0 +1,39 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import controller.Controller;
+import model.*;
+import view.*;
+
+
+public class RicercaPerAttributo {
+	Model model = new Model();
+	MyView view = new BibliotecaView();
+	
+	Controller controller = new Controller(model,view);
+	
+	@Before
+	public void inizializzazione() {
+		Calendar data_iscrizione = Calendar.getInstance();
+		Calendar data_scadenza = Calendar.getInstance();
+		data_scadenza.add(Calendar.YEAR, 5);
+		
+		model.addFruitore("a", "a", 33, "a", "a", data_iscrizione, data_scadenza);
+		
+		
+	}
+	
+
+	@Test
+	public void test() {
+		//controller.usaFruitore("a"); questo metodo stampa a video cosa che non ha senso nel test
+		
+	}
+
+}
